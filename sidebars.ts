9f5691fe0dbc3b2828aae65d977df30f6a642f0c
wagmitt <wagmitt@gmail.com>
2025-05-13 08:06:23 +0700

    fix

--- sidebars.ts
+++ sidebars.ts
@@ -70,13 +70,13 @@ const sidebars: SidebarsConfig = {
         'security/security-audits',
       ],
     },
-    {
-      type: 'category',
-      label: '🎮 EVENTS',
-      items: [
-        // 'events/testnet-campaign',
-      ],
-    },
+    // {
+    //   type: 'category',
+    //   label: '🎮 EVENTS',
+    //   items: [
+    //     'events/testnet-campaign',
+    //   ],
+    // },
     {
       type: 'category',
       label: '💻 DEVELOPERS',
